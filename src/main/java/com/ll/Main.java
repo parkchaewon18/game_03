6b2a2b3ec681ace90cdf07c91d00eec48b385468
parkchaewon18 <0101angela@naver.com>
2023-03-07 09:40:51 +0900

    수동형변환

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,56 +1,18 @@
 package com.ll;
-// 문제 : 매개변수를 사용해서 전사가 매번 다르게 공격하도록 해주세요.
 
+// 문제 : 자동차 리모콘이 페라리 객체를 가리키게 한 후 해당 리모콘이 가리키고 있는 객체를 다시 페라리 리모콘으로 가리키게(참조하게) 하는 코드를 작성해주세요.
 public class Main {
     public static void main(String[] args) {
-        전사 a전사 = new 전사();
 
-        a전사.공격("브라이언", "칼");
-        // 브라이언이(가) 칼(으)로 공격합니다.
+        자동차 a자동차 = new 페라리();
+        페라리 a페라리 = (페라리)a자동차();  //수동형변환
 
-        a전사.재공격();
-        // 브라이언이(가) 칼(으)로 공격합니다.
-
-        a전사.공격("필립", "창");
-        // 필립이(가) 창(으)로 공격합니다.
-
-        a전사.공격("마크", "지팡이");
-        // 마크(가) 지팡이(으)로 공격합니다.
     }
 }
-
-class 전사 {
-    String lastName;
-    String lastWeaponName;
-
-    void 공격(String name, String weaponName) {
-        System.out.println(name + "이(가) " + weaponName + "(으)로 공격합니다.");
-
-        lastName = name;
-        lastWeaponName = weaponName;
-    }
-    void 재공격() {
-        System.out.println(lastName + "이(가) " + lastWeaponName + "(으)로 공격합니다.");
-    }
+class 자동차 {
+    void 달리다() {}
+    void 서다() {}
 }
-
-/* 중복제거 버전
-class 전사 {
-    String lastName;
-    String lastWeaponName;
-
-    void 공격(String name, String weaponName) {
-        공격문구출력(name, weaponName);
-
-        lastName = name;
-        lastWeaponName = weaponName;
-    }
-    void 공격문구출력(String name, String weaponName) {
-        System.out.println(name + "이(가) " + weaponName + "(으)로 공격합니다.");
-    }
-    void 재공격() {
-        공격문구출력(lastName, lastWeaponName);
-    }
+class 페라리 extends 자동차 {
+    void 뚜껑이_열리다() {}
 }
- */
-
